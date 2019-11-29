88ab13babe2a6bb662fab0bb293ca4bcf5f5bf80
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-11-29 02:07:17 -0300

    c_piscine_c_03 Add ft_strstr exercise - 04
    - The function returns the current position from first place where
    the substring was found
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

---
+++ ex04/ft_strstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/28 23:06:25 by ecerquei          #+#    #+#             */
+/*   Updated: 2019/11/29 01:39:16 by ecerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int i;
+	int cursor;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		cursor = 0;
+		while (str[i + cursor] == to_find[cursor] || to_find[cursor] == '\0')
+		{
+			if (to_find[cursor] == '\0')
+			{
+				return (&str[i]);
+			}
+			cursor++;
+		}
+		i++;
+	}
+	return (0);
+}
