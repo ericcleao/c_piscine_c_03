7aa3d5d3a802d38c33089c5bbca9c26c9acd0174
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-12-02 12:46:01 -0300

    c_piscine_c_03 Fix ft_strcmp exercise - 00
    - Update wrong condition when comparer two strings
    - Set the return as the difference from the characters
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,24 +6,24 @@
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/28 08:52:51 by ecerquei          #+#    #+#             */
-/*   Updated: 2019/11/29 01:11:20 by ecerquei         ###   ########.fr       */
+/*   Updated: 2019/12/02 12:43:04 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int i;
+	unsigned int	i;
+	unsigned char	left;
+	unsigned char	right;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] < s2[i])
+		left = (unsigned char)s1[i];
+		right = (unsigned char)s2[i];
+		if ((left < right) || (right > left))
 		{
-			return (-1);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (1);
+			return (left - right);
 		}
 		i++;
 	}
