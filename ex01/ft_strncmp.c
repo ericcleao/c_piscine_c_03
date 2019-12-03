913dd393d9c3bcffada1661ac1553a7c1b159bf9
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-12-03 09:05:30 -0300

    c_piscine_c_03 Fix a wrong condition at the order condition - ex01
    - Set the correct one
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/30 10:00:39 by ecerquei          #+#    #+#             */
-/*   Updated: 2019/12/02 12:42:24 by ecerquei         ###   ########.fr       */
+/*   Updated: 2019/12/02 21:38:38 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	{
 		left = (unsigned char)s1[i];
 		right = (unsigned char)s2[i];
-		if ((left < right) || (right > left))
+		if ((left < right) || (right < left))
 		{
 			return (left - right);
 		}
