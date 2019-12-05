8f33574c7ef102f1c0faa375c3cbc33c871296f9
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-12-05 18:43:31 -0300

    c_piscine_c_05 Fix exercise ft_strlcat - 05
    - Check if size is lower them dest size before put a '\0' at the end of
    it
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 00:56:06 by ecerquei          #+#    #+#             */
-/*   Updated: 2019/12/03 09:09:01 by ecerquei         ###   ########.fr       */
+/*   Updated: 2019/12/05 18:40:11 by ecerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,18 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	unsigned int i;
 	unsigned int dest_size;
 
-	i = 0;
-	while (dest[i] != '\0' && i < size)
+	dest_size = 0;
+	while (dest[dest_size] != '\0' && dest_size < size)
 	{
-		i++;
+		dest_size++;
 	}
-	dest_size = i;
+	i = dest_size;
 	while (src[i - dest_size] != '\0' && (i + 1) < size)
 	{
 		dest[i] = src[i - dest_size];
 		i++;
 	}
-	dest[i] = '\0';
+	if (size > dest_size)
+		dest[i] = '\0';
 	return (dest_size + ft_strlen(src));
 }
